16dfb45aaa174bff8fac2569ab37b1a2fd704cb9
jasenmichael <jasen@ready-builder.com>
2020-12-28 12:24:14 -0600

    chore: style nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,51 +1,77 @@
 <template>
-  <nav
-    class="shadow text-gray-300"
-    :class="
-      scrollY >= yTrigger
-        ? 'bg-gray-800 fixed w-screen z-10'
-        : 'bg-gray-900 py-5'
-    "
-  >
-    <div
-      class="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center"
+  <div>
+    <nav
+      class="transition-all w-screen fixed duration-900 z-50 ease-in-out text-gray-300"
+      :class="
+        $route.path !== '/' || scrollY >= yTrigger || menu // : bg-gray-900
+          ? 'bg-gray-800 shadow-lg'
+          : 'md:py-5'
+      "
     >
-      <div class="flex justify-between items-center">
-        <!-- brand -->
-        <div>
-          <nuxt-link
-            class="text-gray-300 italic font-thin text-2xl md:text-3xl lg:text-4xl hover:text-gray-400"
-            to="/"
-          >
-            {{ $config.siteData.name }}
-          </nuxt-link>
-        </div>
-
-        <!-- mobile menu -->
-        <div class="flex md:hidden" @click="menu = !menu">
-          <div class="w-6 h-6 bg-white ml-14 rounded-md"></div>
-        </div>
-      </div>
-
-      <!-- desktop links -->
       <div
-        class="transition duration-500 ease-in-out md:flex items-center"
-        :class="menu ? 'pt-2 block' : 'hidden'"
+        class="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center"
       >
-        <div
-          class="flex flex-col md:flex-row md:mx-6 space-y-1 md:space-x-6 md:space-y-0"
-        >
-          <nuxt-link
-            v-for="(post, i) in $config.navigation.posts"
-            :key="i"
-            :to="post.path"
-            class="text-sm md:text-base lg:text-lg text-gray-200 font-medium hover:text-indigo-500"
-            >{{ post.title }}</nuxt-link
+        <div class="flex justify-between items-center">
+          <!-- brand -->
+          <div class="z-10 brand" @click="menu = false">
+            <nuxt-link
+              class="text-gray-300 italic font-serif font-thin text-2xl md:text-3xl lg:text-4xl hover:text-gray-400"
+              to="/"
+            >
+              {{ $config.siteData.name }}
+            </nuxt-link>
+          </div>
+
+          <!-- mobile menu -->
+          <div
+            class="md:hidden tham tham-e-squeeze tham-w-6"
+            :class="menu && 'tham-active'"
+            @click="menu = !menu"
           >
+            <div class="tham-box">
+              <div class="tham-inner bg-gray-100" />
+            </div>
+          </div>
+        </div>
+
+        <!-- desktop links -->
+        <div
+          class="block transition-all duration-900 ease-in-out md:flex items-center"
+          :class="menu ? 'mt-2' : 'hidden'"
+        >
+          <div
+            class="flex flex-col md:flex-row md:mx-6 space-y-1 md:space-x-6 md:space-y-0"
+            @click="menu = false"
+          >
+            <!-- pages -->
+            <nuxt-link
+              v-for="(page, i) in $config.navigation.pages"
+              :key="i"
+              :to="page.path"
+              class="text-sm md:text-base lg:text-lg text-gray-200 font-medium hover:text-gray-400 transform"
+              >{{ page.title }}</nuxt-link
+            >
+            <!-- posts -->
+            <nuxt-link
+              v-for="(post, i) in $config.navigation.posts"
+              :key="i"
+              :to="post.path"
+              class="text-sm md:text-base lg:text-lg text-gray-200 font-medium hover:text-gray-400 transform"
+              >{{ post.title }}</nuxt-link
+            >
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+
+    <!-- transparent layer when menu open -->
+    <button
+      v-if="menu"
+      tabindex="-1"
+      class="fixed w-full h-full inset-0 cursor-default z-0 bg-black opacity-50"
+      @click="menu = false"
+    ></button>
+  </div>
 </template>
 
 <script>
@@ -53,7 +79,7 @@ export default {
   data: () => {
     return {
       scrollY: 0,
-      yTrigger: 1,
+      yTrigger: 20,
       menu: false,
     }
   },
@@ -67,3 +93,14 @@ export default {
   },
 }
 </script>
+
+<style lang="stylus" scoped>
+.brand {
+  background-color: white;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  text-shadow: rgba(255, 255, 255, 0.5) 0 3px 3px;
+}
+</style>
